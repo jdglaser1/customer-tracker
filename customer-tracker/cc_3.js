1949d17834940ec6eb79483ddfea25a3f4b74373
Joshua Glaser <jdglaser@usf.edu>
2025-09-10 14:37:03 -0400

    updated with customers array

--- customer-tracker/cc_3.js
+++ customer-tracker/cc_3.js
@@ -1,1 +1,19 @@
-//Testing to see if this uploads
+let customers = [
+{
+    name:"Sally",
+    email: "Sally@gmail.com",
+    purchases: ["Burger", "Fries"]
+},
+
+{
+    name:"Joe",
+    email: "Joe@gmail.com",
+    purchases: ["Salad", "Steak"]
+},
+{
+    name:"Amy",
+    email: "Amy@gmail.com",
+    purchases: ["Milkshake", "Chicken Nuggets"]
+}
+
+]
